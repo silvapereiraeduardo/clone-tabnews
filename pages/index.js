e4229f25e24bdeb0d30e5e9e2385d560d728d1a6
Eduardo Pereira <eduardo.pereira@compasso.com.br>
2024-12-02 11:30:15 -0300

    adicionando .gitignore

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,5 @@
 function Home() {
-    return <body style={{ display: "flex", flexDirection: 'column', minHeight: '100vh', alignItems: 'center', justifyContent: 'center', padding: '15px' }}>
-        <h1>Olá meu amor,</h1>
-        <p>Na aula de hoje com o filipedeschamps foi passado um deseafio, para subir uma página web para alguém especial, no qual escolhi você!
-        </p>
-        <p>Eu te amo muito, e sou muito feliz por ter você ao meu lado, sempre me apoiando e caminhando junto a mim. Te quero sempre, e darei sempre o meu melhor, para ver o seu sorriso lindo!</p>
-        <p>Te amo, e bora fazer nosso churras!</p>
-        <p>{`<3`}</p>
-    </body>
+  return <h1>Teste 4</h1>;
 }
 
 export default Home;
