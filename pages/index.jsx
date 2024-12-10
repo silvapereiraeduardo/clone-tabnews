ded37098cb119f28e915581cb2c0587c18b02a6d
Eduardo Pereira <eduardo.pereira@compasso.com.br>
2024-12-10 15:57:42 -0300

    Pagina em construcao

---
+++ pages/index.jsx
@@ -0,0 +1,33 @@
+function Home() {
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "90vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div
+        style={{
+          maxWidth: "80%",
+        }}
+      >
+        <h1>
+          Esse é um clone do projeto Tabnews, do qual estou aplicando todo o
+          conhecimento recebido no curso.dev.
+        </h1>
+        <a
+          href="https://github.com/silvapereiraeduardo"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          https://github.com/silvapereiraeduardo
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
