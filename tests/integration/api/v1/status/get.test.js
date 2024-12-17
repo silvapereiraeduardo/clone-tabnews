63229bd00fd075d410da6cab3d97f650e76f8fa4
Eduardo Pereira <eduardo.pereira@compasso.com.br>
2024-12-17 11:26:13 -0300

    test to validate database version

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -7,7 +7,13 @@ test("GET to /api/v1/status should return 200", async () => {
   const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
-  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.version).toBeDefined();
+  const databaseVersion = String(
+    responseBody.dependencies.database.version,
+  ).split(".");
+  const databaseMajorVersion = databaseVersion[0];
+  expect(databaseMajorVersion).toEqual("16");
+
   expect(responseBody.dependencies.database.max_connections).toEqual(100);
   expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
